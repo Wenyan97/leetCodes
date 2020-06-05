19ddda46014d122fc5c380787ae2952259e0a6e4
PC_YWY <yanwenyan@airchina.com>
2020-06-05 16:06:46 +0800

    project update

--- src/main/java/com/datastructures/TreeNode.java
+++ src/main/java/com/datastructures/TreeNode.java
@@ -4,9 +4,9 @@ package com.datastructures;
  * @author PC_YWY
  */
 public class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
 
     public TreeNode(int val) {
         this.val = val;
