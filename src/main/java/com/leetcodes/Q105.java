1c777334d3f20d072352d27513d0c03987fc6183
PC_YWY <yanwenyan@airchina.com>
2020-06-09 15:54:27 +0800

    project update

--- src/main/java/com/leetcodes/Q105.java
+++ src/main/java/com/leetcodes/Q105.java
@@ -1,51 +1,55 @@
 package com.leetcodes;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 根据一棵树的前序遍历与中序遍历构造二叉树。
  * 注意:
  * 你可以假设树中没有重复的元素。
- *
+ * <p>
  * 例如，给出
  * 前序遍历 preorder = [3,9,20,15,7]
  * 中序遍历 inorder = [9,3,15,20,7]
  * 返回如下的二叉树：
- *
- *     3
- *    / \
- *   9  20
- *     /  \
- *    15   7
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
  */
 public class Q105 {
-    public TreeNode buildTree(int[] preorder, int[] inorder) {
-        return buildTreeHelper(preorder, 0, preorder.length, inorder, 0, inorder.length);
-    }
-
-    public TreeNode buildTreeHelper(int[] preorder, int p_start, int p_end,
-                                    int[] inorder, int i_start, int i_end) {
-        // preorder 为空，直接返回 null
-        if (p_start == p_end) {
+    public TreeNode myBuildTree(int[] preorder, int[] inorder,
+                                int preorder_left, int preorder_right,
+                                int inorder_left, int inorder_right, Map<Integer, Integer> indexMap) {
+        if (preorder_left > preorder_right) {
             return null;
         }
-        int root_val = preorder[p_start];
-        TreeNode root = new TreeNode(root_val);
-        //在中序遍历中找到根节点的位置
-        int i_root_index = 0;
-        for (int i = i_start; i < i_end; i++) {
-            if (root_val == inorder[i]) {
-                i_root_index = i;
-                break;
-            }
-        }
-        int leftNum = i_root_index - i_start;
-        //递归的构造左子树
-        root.left = buildTreeHelper(preorder, p_start + 1, p_start + leftNum + 1, inorder, i_start, i_root_index);
-        //递归的构造右子树
-        root.right = buildTreeHelper(preorder, p_start + leftNum + 1, p_end, inorder, i_root_index + 1, i_end);
+        // 前序遍历中的第一个节点就是根节点
+        int preorder_root = preorder_left;
+        // 在中序遍历中定位根节点
+        int inorder_root = indexMap.get(preorder[preorder_root]);
+        // 先把根节点建立出来
+        TreeNode root = new TreeNode(preorder[preorder_root]);
+        // 得到左子树中的节点数目
+        int size_left_subtree = inorder_root - inorder_left;
+        // 递归地构造左子树，并连接到根节点
+        // 先序遍历中「从 左边界+1 开始的 size_left_subtree」个元素就对应了中序遍历中「从 左边界 开始到 根节点定位-1」的元素
+        root.left = myBuildTree(preorder, inorder, preorder_left + 1, preorder_left + size_left_subtree, inorder_left, inorder_root - 1, indexMap);
+        // 递归地构造右子树，并连接到根节点
+        // 先序遍历中「从 左边界+1+左子树节点数目 开始到 右边界」的元素就对应了中序遍历中「从 根节点定位+1 到 右边界」的元素
+        root.right = myBuildTree(preorder, inorder, preorder_left + size_left_subtree + 1, preorder_right, inorder_root + 1, inorder_right, indexMap);
         return root;
     }
 
-    public static void main(String[] args) {
-
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int n = preorder.length;
+        // 构造哈希映射，帮助我们快速定位根节点
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return myBuildTree(preorder, inorder, 0, n - 1, 0, n - 1, indexMap);
     }
 }
