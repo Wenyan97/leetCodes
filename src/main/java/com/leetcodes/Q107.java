d6c1f12472866a6a7ac2277fa54b5f6e4def6ebc
PC_YWY <yanwenyan@airchina.com>
2020-06-10 10:51:34 +0800

    Q107 update

---
+++ src/main/java/com/leetcodes/Q107.java
@@ -0,0 +1,52 @@
+package com.leetcodes;
+
+import java.util.*;
+
+/**
+ * 给定一个二叉树，返回其节点值自底向上的层次遍历。 （即按从叶子节点所在层到根节点所在的层，逐层从左向右遍历）
+ * <p>
+ * 例如：
+ * 给定二叉树 [3,9,20,null,null,15,7],
+ * <p>
+ * 3
+ * / \
+ * 9  20
+ * /  \
+ * 15   7
+ * 返回其自底向上的层次遍历为：
+ * <p>
+ * [
+ * [15,7],
+ * [9,20],
+ * [3]
+ * ]
+ */
+public class Q107 {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> resList = new ArrayList<>();
+        //加入头节点
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            List<Integer> levelList = new ArrayList<>();
+            int len = queue.size();
+            for (int i = 0; i < len; i++) {
+                TreeNode node = queue.poll();
+                levelList.add(node.val);
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+            resList.add(levelList);
+        }
+        //反转数组
+        Collections.reverse(resList);
+        return resList;
+    }
+
+
+}
