0a67fa8823db4eff12d7a7281df8e8f47a5f74da
PC_YWY <yanwenyan@airchina.com>
2020-07-14 15:40:21 +0800

    后序遍历

--- src/main/java/com/leetcode/Tree/Q145.java
+++ src/main/java/com/leetcode/Tree/Q145.java
@@ -1,11 +1,32 @@
 package com.leetcode.Tree;
 
 import com.datastructures.TreeNode;
+import org.w3c.dom.html.HTMLLinkElement;
+import sun.awt.image.ImageWatched;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 public class Q145 {
     public List<Integer> postorderTraversal(TreeNode root) {
-
+        Stack<TreeNode> stack = new Stack<>();
+        LinkedList<Integer> ans = new LinkedList<>();
+        if (root == null) {
+            return ans;
+        }
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            TreeNode tmp = stack.pop();
+            ans.addFirst(tmp.val);
+            if (tmp.left != null) {
+                stack.add(tmp.left);
+            }
+            if (tmp.right != null) {
+                stack.add(tmp.right);
+            }
+        }
+        return ans;
     }
 }
