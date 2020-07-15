be47a2797357b7c07287f9cd83460bf86719c82a
PC_YWY <yanwenyan@airchina.com>
2020-07-15 14:02:35 +0800

    Invert Binary Tree

---
+++ src/main/java/com/leetcode/Tree/Q226.java
@@ -0,0 +1,32 @@
+package com.leetcode.Tree;
+
+import com.datastructures.TreeNode;
+
+/**
+ * Invert a binary tree.
+ * Example:
+ * Input:
+ *
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * Output:
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ */
+public class Q226 {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return null;
+        TreeNode tmp = invertTree(root.left);
+        root.left = invertTree(root.right);
+        root.right = tmp;
+        /*root.left = invertTree(root.left);
+        root.right = invertTree(root.right);*/
+        return root;
+    }
+}
