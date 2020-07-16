6dedf41fc58b89e864d6e7cb25e5123100331c16
PC_YWY <yanwenyan@airchina.com>
2020-07-16 09:48:09 +0800

    Path Sum II Sum Root to Leaf Numbers

---
+++ src/main/java/com/leetcode/Tree/Q113.java
@@ -0,0 +1,53 @@
+package com.leetcode.Tree;
+
+import com.datastructures.TreeNode;
+import com.sun.org.apache.bcel.internal.generic.IF_ACMPEQ;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Q113 {
+    List<List<Integer>> ans = new ArrayList<>();
+    List<Integer> list = new ArrayList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if (root != null) {
+            dfs(root, sum);
+        }
+        return ans;
+    }
+
+    public void dfs(TreeNode root, int sum) {
+        if (root == null) return;
+        list.add(root.val);
+        if (root.left == null && root.right == null ) {
+            if (sum == root.val) {
+                ans.add(new ArrayList(list));
+            } else {
+                list.remove(list.size() - 1);
+                return;
+            }
+        }
+        dfs(root.left, sum - root.val);
+        dfs(root.right, sum - root.val);
+        list.remove(list.size() - 1);
+    }
+    public void dfs(TreeNode root, int sum, List<List<Integer>> res, List<Integer> path){
+        if(root==null) return;
+        path.add(root.val);
+
+        if(root.left==null && root.right==null ){
+            if(root.val==sum)
+                res.add(new ArrayList<Integer>(path));
+            return;
+        }
+        if(root.left!=null) {
+            dfs(root.left,sum-root.val,res,path);
+            path.remove(path.size()-1);
+        }
+        if(root.right!=null) {
+            dfs(root.right,sum-root.val,res,path);
+            path.remove(path.size()-1);
+        }
+
+    }
+}
