8ea1fcde79b8780be5fc4ebe772deb3e685aab90
PC_YWY <yanwenyan@airchina.com>
2020-07-16 16:58:32 +0800

    Minimum Depth of Binary Tree

---
+++ src/main/java/com/leetcode/Tree/Q111.java
@@ -0,0 +1,29 @@
+package com.leetcode.Tree;
+
+import com.datastructures.TreeNode;
+
+/**
+ * Given a binary tree, find its minimum depth.
+ *
+ * The minimum depth is the number of nodes along the shortest path from the root node down to the nearest leaf node.
+ *
+ * Note: A leaf is a node with no children.
+ */
+public class Q111 {
+    int minDepth = Integer.MAX_VALUE;
+    public int minDepth(TreeNode root) {
+        if (root == null) return 0;
+        dfs(root, 0);
+        return minDepth;
+    }
+
+    public void dfs(TreeNode root, int currDepth) {
+        if (root == null) return;
+        currDepth++;
+        if (root.left == null && root.right == null && currDepth < minDepth) {
+            minDepth = currDepth;
+        }
+        dfs(root.left, currDepth);
+        dfs(root.right, currDepth);
+    }
+}
