a5437bda89cb04ebaff59cdcf03d944eabc7e3a3
wenyan <820974858@qq.com>
2020-07-23 10:41:53 +0800

    Inorder Successor in BST

---
+++ src/main/java/com/leetcode/Tree/BST/InorderSuccessorInBST.java
@@ -0,0 +1,28 @@
+package com.leetcode.Tree.BST;
+
+import com.datastructures.TreeNode;
+
+import java.util.Stack;
+
+public class InorderSuccessorInBST {
+    public TreeNode inorderSuccessor(TreeNode root, TreeNode p) {
+        // write your code here
+        if (root == null) return null;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        boolean flag = false;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            if (flag) return curr;
+            if (curr.val == p.val) {
+                flag = true;
+            }
+            curr = curr.right;
+        }
+        return null;
+    }
+}
---
+++ src/main/java/com/leetcode/Tree/BST/SerializeAndDeserializeBinaryTree.java
@@ -0,0 +1,4 @@
+package com.leetcode.Tree.BST;
+
+public class SerializeAndDeserializeBinaryTree {
+}
