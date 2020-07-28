8d6e7611a880d463616faa7e0b7a6231f98691ea
wenyan <820974858@qq.com>
2020-07-28 15:23:59 +0800

    update

--- src/main/java/com/leetcode/Array/RemoveElement.java
+++ src/main/java/com/leetcode/Array/RemoveElement.java
@@ -5,7 +5,7 @@ public class RemoveElement {
         int len = nums.length;
         for (int i = 0; i < len; i++) {
             if (nums[i] == val) {
-                for (int j = i; j < len; j++) {
+                for (int j = i; j < len - 1; j++) {
                     nums[j] = nums[j + 1];
                 }
                 len--;
