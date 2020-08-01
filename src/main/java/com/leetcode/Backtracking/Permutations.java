b2c239f1f811508326e84c81c08d0d314239987b
wenyan <820974858@qq.com>
2020-08-01 17:39:51 +0800

    permutations

---
+++ src/main/java/com/leetcode/Backtracking/Permutations.java
@@ -0,0 +1,52 @@
+package com.leetcode.Backtracking;
+
+import java.security.Permission;
+import java.util.*;
+
+/**
+ * Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+ *
+ * Example:
+ *
+ * Input: [1,1,2]
+ * Output:
+ * [
+ *   [1,1,2],
+ *   [1,2,1],
+ *   [2,1,1]
+ * ]
+ */
+public class Permutations {
+    List<List<Integer>> ans = new ArrayList<>();
+    public List<List<Integer>> permute(int[] nums) {
+        if (nums.length == 0) return ans;
+        backtrack(nums, new ArrayList<>(), new HashSet<>());
+        return ans;
+    }
+
+    //distinct可以用boolean used[i] 代替
+    public void backtrack(int[] nums, List<Integer> path, Set<Integer> distinct) {
+        if (path.size() == nums.length) {
+            ans.add(new ArrayList<>(path));
+        }
+        for (int num : nums) {
+            if (!distinct.contains(num)) {
+                path.add(num);
+                //set用于判断是否重复
+                distinct.add(num);
+                backtrack(nums, path, distinct);
+                //删除元素
+                path.remove(path.size() - 1);
+                distinct.remove(num);
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Permutations so = new Permutations();
+        int[] a = {1, 2, 3};
+        so.permute(a);
+        System.out.println(so.ans);
+    }
+}
