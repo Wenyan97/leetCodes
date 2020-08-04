e917a64f66bc81026a5d1a809480e700c99a8771
PC_YWY <yanwenyan@airchina.com>
2020-08-04 10:45:24 +0800

    swap nodes in pairs

---
+++ src/main/java/com/leetcode/LinkedList/SwapNodesInPairs.java
@@ -0,0 +1,28 @@
+package com.leetcode.LinkedList;
+
+import com.datastructures.ListNode;
+
+import java.util.List;
+
+/**
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ *
+ * You may not modify the values in the list's nodes, only nodes itself may be changed.
+ *
+ *
+ *
+ * Example:
+ *
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ */
+public class SwapNodesInPairs {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode first = head, second = head.next;
+
+        ListNode remain = swapPairs(second.next);
+        first.next = remain;
+        second.next = first;
+        return second;
+    }
+}
