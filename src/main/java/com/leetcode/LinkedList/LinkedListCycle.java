346938afefb34ed1b833185ea4ae9d84d40b5d79
PC_YWY <yanwenyan@airchina.com>
2020-08-04 10:34:10 +0800

    linked list cycle

---
+++ src/main/java/com/leetcode/LinkedList/LinkedListCycle.java
@@ -0,0 +1,22 @@
+package com.leetcode.LinkedList;
+
+import com.datastructures.ListNode;
+
+import java.util.List;
+
+/**
+ * Given a linked list, determine if it has a cycle in it.
+ *
+ * To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+ */
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) return true;
+        }
+        return false;
+    }
+}
