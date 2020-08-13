a584610d86cbaec0388ad12230974a534e8a6366
wenyan <820974858@qq.com>
2020-08-13 14:50:31 +0800

    中序和后序遍历

---
+++ src/main/java/com/leetcode3/tree/BinaryTreeInorderTraversal.java
@@ -0,0 +1,26 @@
+package com.leetcode3.tree;
+
+import com.datastructures.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class BinaryTreeInorderTraversal {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if (root == null) return list;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            curr = stack.pop();
+            list.add(curr.val);
+            curr = curr.right;
+        }
+        return list;
+    }
+}
---
+++ src/main/java/com/leetcode3/tree/BinaryTreePostorderTraversal.java
@@ -0,0 +1,4 @@
+package com.leetcode3.tree;
+
+public class BinaryTreePostorderTraversal {
+}
