1e6cb2efe0bf0cde804be7cf50d391c16b5a2c35
wenyan <820974858@qq.com>
2020-08-24 10:04:41 +0800

    HouseRobberIII and ValidateBinarySearchTree

---
+++ src/main/java/com/leetcode3/tree/HouseRobberIII.java
@@ -0,0 +1,67 @@
+package com.leetcode3.tree;
+
+import com.datastructures.TreeNode;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * The thief has found himself a new place for his thievery again. There is only one entrance to this area, called the "root." Besides the root, each house has one and only one parent house. After a tour, the smart thief realized that "all houses in this place forms a binary tree". It will automatically contact the police if two directly-linked houses were broken into on the same night.
+ * <p>
+ * Determine the maximum amount of money the thief can rob tonight without alerting the police.
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: [3,2,3,null,3,null,1]
+ * <p>
+ * 3
+ * / \
+ * 2   3
+ * \   \
+ * 3   1
+ * <p>
+ * Output: 7
+ * Explanation: Maximum amount of money the thief can rob = 3 + 3 + 1 = 7.
+ */
+public class HouseRobberIII {
+    public int rob(TreeNode root) {
+        HashMap<TreeNode, Integer> memo = new HashMap<>();
+        return robInternal(root, memo);
+    }
+
+    public int robInternal(TreeNode root, HashMap<TreeNode, Integer> memo) {
+        if (root == null) return 0;
+        if (memo.containsKey(root)) return memo.get(root);
+        int money = root.val;
+
+        if (root.left != null) {
+            money += (robInternal(root.left.left, memo) + robInternal(root.left.right, memo));
+        }
+        if (root.right != null) {
+            money += (robInternal(root.right.left, memo) + robInternal(root.right.right, memo));
+        }
+        int result = Math.max(money, robInternal(root.left, memo) + robInternal(root.right, memo));
+        memo.put(root, result);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        TreeNode node1 = new TreeNode(3);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(5);
+        TreeNode node4 = new TreeNode(5);
+        TreeNode node5 = new TreeNode(3);
+        TreeNode node6 = new TreeNode(2);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node2.right = node5;
+        node3.right = node6;
+        HouseRobberIII so = new HouseRobberIII();
+        List<Integer> list = new LinkedList<>();
+        System.out.println(so.rob(node1));
+
+    }
+}
