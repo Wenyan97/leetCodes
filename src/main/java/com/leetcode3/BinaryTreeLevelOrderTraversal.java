ef4c3234505fc9ce3326c8b4421d6eacf806871e
wenyan <820974858@qq.com>
2020-12-07 23:00:02 +0800

    update

--- src/main/java/com/leetcode3/BinaryTreeLevelOrderTraversal.java
+++ src/main/java/com/leetcode3/BinaryTreeLevelOrderTraversal.java
@@ -1,7 +1,6 @@
 package com.leetcode3;
 
 import com.datastructures.TreeNode;
-import com.sun.source.tree.Tree;
 
 import java.util.ArrayList;
 import java.util.LinkedList;
---
+++ src/main/java/com/leetcode3/SymmetricTree.java
@@ -0,0 +1,19 @@
+package com.leetcode3;
+
+import com.datastructures.TreeNode;
+
+public class SymmetricTree {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return helper(root, root);
+    }
+
+
+    public boolean helper(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        return root1.val == root2.val
+                && helper(root1.left, root2.right)
+                && helper(root1.right, root2.left);
+    }
+}
