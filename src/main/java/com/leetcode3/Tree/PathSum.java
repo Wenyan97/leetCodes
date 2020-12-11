30adc955016c623a393b586dab6ae1263792db16
wenyan <820974858@qq.com>
2020-12-11 16:12:07 +0800

    path sum

---
+++ src/main/java/com/leetcode3/Tree/PathSum.java
@@ -0,0 +1,18 @@
+package com.leetcode3.Tree;
+
+import com.datastructures.TreeNode;
+
+/**
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ */
+public class PathSum {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null) return false;
+        if (root.left == null && root.right == null) {
+            if (root.val == sum) {
+                return true;
+            } else return false;
+        }
+        return hasPathSum(root.left, sum - root.val) || hasPathSum(root.right, sum - root.val);
+    }
+}
