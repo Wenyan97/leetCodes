6b253acc8af9c75e8c7adc71e783355a86301ae2
wenyan <820974858@qq.com>
2020-12-11 16:38:27 +0800

    path sum II

---
+++ src/main/java/com/leetcode3/Tree/PathSumII.java
@@ -0,0 +1,55 @@
+package com.leetcode3.Tree;
+
+import com.datastructures.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PathSumII {
+    List<List<Integer>> res = new ArrayList<>();
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        if (root == null) return res;
+        dfs(root, sum, new ArrayList<>());
+        return res;
+    }
+
+    public void dfs(TreeNode root, int sum, List<Integer> list) {
+        if (root == null) return;
+        list.add(root.val);
+        if (root.left == null && root.right == null) {
+            if (root.val == sum) {
+                res.add(new ArrayList<>(list));
+            }
+            list.remove(list.size() - 1);
+            return;
+        }
+        dfs(root.left, sum - root.val, list);
+        dfs(root.right, sum - root.val, list);
+        list.remove(list.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        PathSumII solution = new PathSumII();
+        TreeNode node1 = new TreeNode(5);
+        TreeNode node2 = new TreeNode(4);
+        TreeNode node3 = new TreeNode(8);
+        TreeNode node4 = new TreeNode(11);
+        TreeNode node5 = new TreeNode(13);
+        TreeNode node6 = new TreeNode(4);
+        TreeNode node7 = new TreeNode(7);
+        TreeNode node8 = new TreeNode(2);
+        TreeNode node9 = new TreeNode(5);
+        TreeNode node10 = new TreeNode(1);
+        node1.left = node2;
+        node1.right = node3;
+        node2.left = node4;
+        node3.left = node5;
+        node3.right = node6;
+        node4.left = node7;
+        node4.right = node8;
+        node6.left = node9;
+        node6.right = node10;
+        List<List<Integer>> list = solution.pathSum(node1, 22);
+        System.out.println("hello!");
+    }
+}
