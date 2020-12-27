b05facdd902a5ea50d77d525490ac3acf4c08c9b
wenyan <820974858@qq.com>
2020-12-27 09:50:20 +0800

    g

--- src/main/java/com/leetcode3/Tree/BFS/BinaryTreeLevelOrderTraversalII.java
+++ src/main/java/com/leetcode3/Tree/BFS/BinaryTreeLevelOrderTraversalII.java
@@ -26,4 +26,11 @@ public class BinaryTreeLevelOrderTraversalII {
         }
         return ans;
     }
+
+    public static void main(String args[]) {
+        System.out.println("hello world!!");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+    }
 }
