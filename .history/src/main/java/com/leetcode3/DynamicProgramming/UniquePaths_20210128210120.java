c4bd3f95fb924db28cf30f3fcddd23036478882c
wenyan <820974858@qq.com>
2021-01-28 21:01:29 +0800

    update

---
+++ .history/src/main/java/com/leetcode3/DynamicProgramming/UniquePaths_20210128210120.java
@@ -0,0 +1,22 @@
+package com.leetcode3.DynamicProgramming;
+
+public class UniquePaths {
+    public int uniquePaths(int m, int n) {
+        int[][] dp = new int[m][n];
+        //第一列都是1
+        for (int i = 0; i < m; i++) {
+            dp[i][0] = 1;
+        }
+        //第一行都是1
+        for (int i = 0; i < n; i++) {
+            dp[0][i] = 1;
+        }
+    
+        //这里是递推公式
+        for (int i = 1; i < m; i++)
+            for (int j = 1; j < n; j++)
+                dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+        return dp[m - 1][n - 1];
+    }
+    
+}
