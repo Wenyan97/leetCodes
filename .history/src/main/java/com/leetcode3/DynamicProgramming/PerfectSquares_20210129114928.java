bd38eaf0e6ad46b9ef13ccae9cdd6b2dee5c19c1
wenyan <820974858@qq.com>
2021-01-29 12:01:47 +0800

    完全平方数

---
+++ .history/src/main/java/com/leetcode3/DynamicProgramming/PerfectSquares_20210129114928.java
@@ -0,0 +1,11 @@
+package com.leetcode3.DynamicProgramming;
+
+public class PerfectSquares {
+    public int numSquares(int n) {
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(11));
+    }
+}
---
+++ .history/src/main/java/com/leetcode3/DynamicProgramming/PerfectSquares_20210129120021.java
@@ -0,0 +1,22 @@
+package com.leetcode3.DynamicProgramming;
+
+import jdk.internal.perf.Perf;
+
+public class PerfectSquares {
+    public int numSquares(int n) {
+        int[] dp = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            // 最坏情况：dp[i] = 1 + 1 + ... + 1
+            dp[i] = i;
+            for (int j = 1; j <= Math.sqrt(i); j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        PerfectSquares solution = new PerfectSquares();
+        System.out.println(solution.numSquares(12));
+    }
+}
