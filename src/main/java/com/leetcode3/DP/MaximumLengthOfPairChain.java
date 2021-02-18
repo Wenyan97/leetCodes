332f9aa7cfbede9283b559c01592507f16fef81f
wenyan <820974858@qq.com>
2021-02-18 18:04:29 +0800

    递增数对

---
+++ src/main/java/com/leetcode3/DP/MaximumLengthOfPairChain.java
@@ -0,0 +1,28 @@
+package com.leetcode3.DP;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class MaximumLengthOfPairChain {
+    public int findLongestChain(int[][] pairs) {
+        if (pairs == null || pairs.length == 0) {
+            return 0;
+        }
+        Arrays.sort(pairs, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b) {
+                return a[0] - b[0];
+            }
+        });
+        int n = pairs.length;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (pairs[j][1] < pairs[i][0]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        return Arrays.stream(dp).max().orElse(0);
+    }
+}
