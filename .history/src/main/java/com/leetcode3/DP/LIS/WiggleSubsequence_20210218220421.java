19a18f144a6498957d21ba561797c4b84ee66cad
wenyan <820974858@qq.com>
2021-02-19 16:27:53 +0800

    update

---
+++ .history/src/main/java/com/leetcode3/DP/LIS/WiggleSubsequence_20210218220421.java
@@ -0,0 +1,41 @@
+package com.leetcode3.DP.LIS;
+
+import java.util.Arrays;
+
+public class WiggleSubsequence {
+    public int wiggleMaxLength(int[] nums) {
+        int len = nums.length;
+        if (nums == null || len == 0) {
+            return 0;
+        }
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+        boolean flag = true; // true: positive; false: negative
+        for (int i = 0; i < len; i++) {
+            if (flag == true) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] - nums[j] > 0) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                        
+                    }
+                }
+                flag = false;    
+            } else {
+                for (int j = 0; j < i; j++) {
+                    if (nums[i] - nums[j] > 0) {
+                        dp[i] = Math.max(dp[i], dp[j] + 1);
+                    }
+                }  
+                flag = true;  
+            }
+            
+        }
+        return Arrays.stream(dp).max().orElse(0);
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 17, 5, 10, 13, 15, 10, 5, 16, 8};
+        WiggleSubsequence solution = new WiggleSubsequence();
+        System.out.println(solution.wiggleMaxLength(nums));
+    }
+}
---
+++ .history/src/main/java/com/leetcode3/DP/LIS/WiggleSubsequence_20210218223011.java
@@ -0,0 +1,33 @@
+package com.leetcode3.DP.LIS;
+
+import java.util.Arrays;
+
+public class WiggleSubsequence {
+    public int wiggleMaxLength(int[] nums) {
+        int len = nums.length;
+        if (nums == null || len == 0) {
+            return 0;
+        }
+        int[] dp = new int[len];
+        Arrays.fill(dp, 1);
+        boolean flag = true; // true: positive; false: negative
+        for (int i = 1; i < len; i++) {
+            flag = true;
+            for (int j = 0; j < i; j++) {
+                if ((nums[i] > nums[j] && flag == true) || (nums[i] < nums[j] && flag == false)) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                    
+                }
+                flag = flag == true ? false : true;
+            }
+        }
+        return Arrays.stream(dp).max().orElse(0);
+    }
+
+    public static void main(String[] args) {
+        // int[] nums = {1, 17, 5, 10, 13, 15, 10, 5, 16, 8};
+        int[] nums = {1, 7, 4, 9, 2, 5};
+        WiggleSubsequence solution = new WiggleSubsequence();
+        System.out.println(solution.wiggleMaxLength(nums));
+    }
+}
