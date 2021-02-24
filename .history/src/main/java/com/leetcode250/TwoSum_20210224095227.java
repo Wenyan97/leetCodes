35270a68dd1cb89f0b4998d21862b53bdf63a844
wenyan <820974858@qq.com>
2021-02-24 09:56:37 +0800

    two sum

---
+++ .history/src/main/java/com/leetcode250/TwoSum_20210224095227.java
@@ -0,0 +1,29 @@
+package com.leetcode250;
+
+import java.util.List;
+
+public class TwoSum {
+    public int[] twoSum(int[] nums, int target) {
+        int[] index = new int[2];
+        if (nums == null || nums.length == 0) {
+            return null;
+        }
+        for (int i = 0; i < index.length - 1; i++) {
+            for (int j = i + 1; j < index.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    index[0] = i;
+                    index[1] = j;
+                    return index;
+                }
+            }
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3,2,4}
+        TwoSum solution = new TwoSum();
+        solution.twoSum(nums, 6);
+    }
+    
+}
