2b32f78c679c23684376b41d339f129669445ee2
wenyan <820974858@qq.com>
2021-03-10 18:05:54 +0800

    update

--- .history/src/main/java/com/leetcode3/Backtracking/BinaryTreePaths_20210310175300.java
+++ .history/src/main/java/com/leetcode3/Backtracking/BinaryTreePaths_20210310175300.java
@@ -33,15 +33,15 @@ public class BinaryTreePaths {
     }
 
     public static void main(String[] args) {
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node2 = new TreeNode(2);
-        TreeNode node3 = new TreeNode(3);
-        TreeNode node4 = new TreeNode(5);
-        node1.left = node2;
-        node1.right = node3;
-        node2.right = node4;
-        BinaryTreePaths solution = new BinaryTreePaths();
-        solution.binaryTreePaths(node1);
+        // TreeNode node1 = new TreeNode(1);
+        // TreeNode node2 = new TreeNode(2);
+        // TreeNode node3 = new TreeNode(3);
+        // TreeNode node4 = new TreeNode(5);
+        // node1.left = node2;
+        // node1.right = node3;
+        // node2.right = node4;
+        // BinaryTreePaths solution = new BinaryTreePaths();
+        // solution.binaryTreePaths(node1);
 
         
         Stack<Character> stack = new Stack<>();
