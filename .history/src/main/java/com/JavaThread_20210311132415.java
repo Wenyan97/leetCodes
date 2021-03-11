f552bc8038181313d9aa1e6c868cf7bb5440f767
wenyan <820974858@qq.com>
2021-03-11 18:23:04 +0800

    update

---
+++ .history/src/main/java/com/JavaThread_20210311132415.java
@@ -0,0 +1,64 @@
+package com;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class JavaThread {
+    
+    public static void main(String[] args) {
+        var q = new TaskQueue();
+        var ts = new ArrayList<Thread>();
+
+        for (int i = 0; i < args.length; i++) {
+            var t = new Thread() {
+                public void run() {
+                    while (true) {
+                        try {
+                            String s = q.getTask();
+                            System.out.println("execute tesk:" + s);
+                        } catch (InterruptedException e) {
+                            //TODO: handle exception
+                            return;
+                        }
+                    }
+                }
+            }
+
+            t.start();
+            ts.add(t);
+        }
+        
+        var add = new Thread(() -> {
+            for (int i=0; i<10; i++) {
+                // 放入task:
+                String s = "t-" + Math.random();
+                System.out.println("add task: " + s);
+                q.addTask(s);
+                try { Thread.sleep(100); } catch(InterruptedException e) {}
+            }
+        });
+        add.start();
+        add.join();
+        Thread.sleep(100);
+        for (var t : ts) {
+            t.interrupt();
+        }
+    }
+}
+
+class TaskQueue {
+    Queue<String> queue = new LinkedList<>();
+    
+    public synchronized void addTask(String s) {
+        this.queue.add(s);
+        this.notifyAll();
+    }
+
+    public synchronized String getTask() throws InterruptedException {
+        while (queue.isEmpty()) {
+            this.wait();
+        }
+        return queue.remove();
+    }
+}
