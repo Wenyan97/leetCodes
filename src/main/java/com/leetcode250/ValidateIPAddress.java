f540026a62f8531ac958f975d6e91e7938c76f8e
wenyan <820974858@qq.com>
2021-03-16 16:02:15 +0800

    update

---
+++ src/main/java/com/leetcode250/ValidateIPAddress.java
@@ -0,0 +1,14 @@
+package com.leetcode250;
+
+public class ValidateIPAddress {
+    public String validIPAddress(String IP) {
+        char[] ipList = IP.toCharArray();
+        return "";
+    }
+
+    public boolean isIPv4(char[] IP) {
+        return true;
+    }
+
+    
+}
